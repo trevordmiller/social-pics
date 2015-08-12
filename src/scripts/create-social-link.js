277b912512ed8d4c982db1485c7fccbd613f4e5b
Trevor D. Miller <trevordmiller@icloud.com>
2015-08-12 11:22:10 -0600

    Step 2

--- src/scripts/create-social-link.js
+++ src/scripts/create-social-link.js
@@ -1,13 +1,3 @@
-export default function createSocialLink(type, url, text, hashtag) {
-  let link = '';
+export default function createSocialLink() {
 
-  if(type === 'twitter') {
-    link = `https://twitter.com/share?url=${url}&text=${text}&hashtags=${hashtag}`;
-  }
-
-  if(type === 'linkedIn') {
-    link = `https://linkedin.com/shareArticle?url=${url}&title=${text}%20%23${hashtag}`;
-  }
-
-  return link;
 }
--- test/create-social-link.js
+++ test/create-social-link.js
@@ -5,29 +5,8 @@ describe('createSocialLink', () => {
 
   describe('#default()', () => {
 
-    let socialData = [
-      'facebook',
-      'www.youtube.com',
-      'Broadcast Yourself',
-      'videos'
-    ];
-
     it('should be a function', () => {
       assert.isFunction(createSocialLink);
-    });
-
-    it('should return a string', () => {
-      assert.isString(createSocialLink());
-    });
-
-    it('should return the twitter link patterns', () => {
-      socialData[0] = 'twitter';
-      assert.equal(createSocialLink(...socialData), 'https://twitter.com/share?url=www.youtube.com&text=Broadcast Yourself&hashtags=videos');
-    });
-
-    it('should return the linkedIn link patterns', () => {
-      socialData[0] = 'linkedIn';
-      assert.equal(createSocialLink(...socialData), 'https://linkedin.com/shareArticle?url=www.youtube.com&title=Broadcast Yourself%20%23videos');
     });
   });
 });
